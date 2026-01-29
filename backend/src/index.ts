fc5c2c5c97f674f2f0ac7caa8710bbd5d89b2e91
Onlyakr <onlyakr2477@gmail.com>
2026-01-29 16:26:40 +0700

    feat: implement radius server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,16 @@
 import { Elysia, status, t } from "elysia";
 import { prisma } from "./lib/prisma";
+import { authenticateUser } from "./services/auth";
+
+import jwt from "@elysiajs/jwt";
 
 const app = new Elysia()
+	.use(
+		jwt({
+			name: "jwt",
+			secret: "Fischl von Luftschloss Narfidort",
+		}),
+	)
 	.get("/", () => "Hello Durain naja.")
 	.get("/durians", async () => {
 		const durians = await prisma.durian.findMany();
@@ -27,6 +36,32 @@ const app = new Elysia()
 		const users = await prisma.user.findMany();
 		return status(200, { users });
 	})
+	.post(
+		"/auth/login",
+		async ({ body, jwt, cookie: { auth } }) => {
+			const isAuthorized = await authenticateUser(body.name, body.password);
+			if (!isAuthorized) {
+				return status(401, { success: false, message: "Invalid credentials" });
+			}
+
+			const value = await jwt.sign({ name: body.name });
+
+			auth.set({
+				value,
+				httpOnly: true,
+				maxAge: 7 * 86400,
+				path: "/",
+			});
+
+			return status(200, { success: true, message: "Logged in successfully" });
+		},
+		{
+			body: t.Object({
+				name: t.String(),
+				password: t.String(),
+			}),
+		},
+	)
 	.listen(8080);
 
 console.log(
